828a1afea38deed501efdee3dc81c0b72fb2db08
user <floby132@gmail.com>
2020-02-13 14:01:03 +0200

    20200213_14-01

---
+++ ievade_idle.py
@@ -0,0 +1,16 @@
+text = print('Ievadiet daudzumu: ')
+n=int(input(text))
+
+x = print('Ievadiet 1 daudzuma vertibu: ')
+input(x)
+
+try:
+    x*x
+    #pamegina, ja sanak, tad sanak, ja ne, tad nemet fatal erroru
+except:
+    "ir kkada kluda"
+
+
+print('vertiba ir: %d'%(x*d))
+
+ #inputs visu sanemto uzskata par tekstu
--- pirmais_idle_skripts.py
+++ pirmais_idle_skripts.py
@@ -12,7 +12,16 @@ for i in range(5):
 
 vaards='Maris'
 uzvaards='Terauds'
+
 #printf("Vards %s", vaards)  ta butu C valoda
 
 print("Vards %s"%(vaards))
 print("Vards, Uzvards: %s %s %d"%(vaards,uzvaards,x))
+
+print("%d * %d = %d"%(x,x,x*x))
+
+b = x*x
+print("%d * %d = %d"%(x,x,b))
+
+c = x**5 #pow(x,5) rezultats double
+print("%d ^ %d = %d"%(x,5,c))
