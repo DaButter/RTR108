d3a727d79f4d1a8f1c579e6d6ca6ba9708b47300
user <floby132@gmail.com>
2020-02-13 13:29:33 +0200

    20200213-13-29

---
+++ pirmais_idle_skripts.py
@@ -0,0 +1,18 @@
+x = 10
+
+if x > 0 :
+    print('x is positive')
+
+if x < 200:
+    print(x)
+print(x*x)
+
+for i in range(5):
+    print(i)
+
+vaards='Maris'
+uzvaards='Terauds'
+#printf("Vards %s", vaards)  ta butu C valoda
+
+print("Vards %s"%(vaards))
+print("Vards, Uzvards: %s %s %d"%(vaards,uzvaards,x))
