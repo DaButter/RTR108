c334c1db6f16d9e8364d5be1f971d5eb6136b890
root <floby132@gmail.com>
2020-08-11 09:52:12 -0400

    20201008-1652

--- laboratorijas_darbi/Taylor_series.py
+++ laboratorijas_darbi/Taylor_series.py
@@ -1,14 +1,12 @@
 import math
-# kas ir cmath? vai var importet atseviskus modulus?
-# vai pirms katras sin darbibas jaraksta math.darbiba?
 
 # vizualizacija SUM bloks
 
 print('\n\n\t   500 \n') # ar \t taulaciju nocentre
 print('\t -------- \n')
-print('\t \\                (k+1)   k   (2k-1)\n') #pakapes
-print("\t  \\          (-1)^     *x^ *2^\n") #dalas augsa
-print(" f(x) =\t   |      ----------------------------   = (sin(sqrt(x)))^2\n") #dalas vidus
+print('\t \\                (k+1)   k   (2k-1)') #pakapes
+print("\t  \\          (-1)^     *x^ *2^") #dalas augsa
+print(" f(x) =\t   |      ----------------------------   = (sin(sqrt(x)))^2") #dalas vidus
 print("  \t  /\t           (2*k)!"); #dalas apaksa
 print("\n\t /\n")
 print("\t -------- \n")
@@ -28,16 +26,29 @@ print('\n') # pievienot ari sin(sqrt(X))^2 kur vizualize x
 
 # SUM darbibas block
 
-print('Izmantojot series summu: ')
-print('\n\n')
+print('Izmantojot series summu: \n')
 
-# iemacities py4e ciklus
+a=x
+Sum=a
+k=1
+
+while k<501:
+    k+=1
+    a=a*(-4)*x/((2*k)*(2*k-1)) # a*R
+    Sum = Sum + a; # SUM
+
+    if k==499:
+        print('Rinda nr.%d. | X=%.5Lg | a=%.5Le | SUM=%.5Lf | \n'%(k,x,a,Sum))
+    if k==500:
+        print('Rinda nr.%d. | X=%.5Lg | a=%.5Le | SUM=%.5Lg | \n'%(k,x,a,Sum))
+
+    
 
 # REKURENCES reizinataja vizualizacijas bloks
 
 print("\nRekurences reizinatajs:\n\n");
 
-print("           (-4)*x\n");
-print(" R = -------------------- \n");
-print("        (2*k)*(2*k-1)\n");
+print("           (-4)*x");
+print(" R = -------------------- ");
+print("        (2*k)*(2*k-1)");
 print("\n");
